e315b8eb52747f1537b75cb4c4bff9fc5d527391
EnzoAlfonso <102090917+EnzoAlfonso@users.noreply.github.com>
2024-11-10 21:09:58 -0300

    feat: fix de pointAssigmentRule

--- app/routes/pointAssignmentRule.routes.js
+++ app/routes/pointAssignmentRule.routes.js
@@ -1,10 +1,11 @@
+// pointAssignmentRule.routes.js
 module.exports = app => {
-    const pointAssignmentRule = require("../controllers/pointAssignmentRule.controller.js");
-    const router = require("express").Router();
-    router.post("/", pointAssignmentRule.create);
-    router.get("/", pointAssignmentRule.findAll);
-    router.get("/:id", pointAssignmentRule.findOne);
-    router.put("/:id", pointAssignmentRule.update);
-    router.delete("/:id", pointAssignmentRule.delete);
-    app.use("/api/pointassignmentrule", router);
+  const pointAssignmentRule = require("../controllers/pointAssignmentRule.controller");
+  const router = require("express").Router();
+  router.post("/", pointAssignmentRule.create);
+  router.get("/", pointAssignmentRule.findAll);
+  router.get("/:id", pointAssignmentRule.findOne);
+  router.put("/:id", pointAssignmentRule.update);
+  router.delete("/:id", pointAssignmentRule.delete);
+  app.use("/api/reglas-asignacion-puntos", router);
 };
